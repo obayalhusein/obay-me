c8684c1715a3e3b16d69d3ae7ac6ac52da92caa5
obayalhusein <obayalhusein@gmail.com>
2021-12-27 19:33:41 +0200

    added sass loader

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,6 +1,11 @@
 <template>
   <Layout>
     <h1>About us</h1>
+    <ul>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+    </ul>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error doloremque omnis animi, eligendi magni a voluptatum, vitae, consequuntur rerum illum odit fugit assumenda rem dolores inventore iste reprehenderit maxime! Iusto.</p>
   </Layout>
 </template>
@@ -12,8 +17,16 @@ export default {
   }
 }
 </script>
-<style scoped>
+<style lang="scss">
 h1 {
   background: red;
 }
+
+ul {
+  background: green;
+  li {
+    color: purple;
+  }
+}
+
 </style>
