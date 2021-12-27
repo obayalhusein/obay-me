1a9020f41d2ee805b2c04e75c7a054d18f4cb4a0
obayalhusein <obayalhusein@gmail.com>
2021-12-27 21:39:43 +0200

    added sass files

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,12 +1,5 @@
 <template>
   <Layout>
-    <h1>About us</h1>
-    <ul>
-      <li>1</li>
-      <li>2</li>
-      <li>3</li>
-    </ul>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error doloremque omnis animi, eligendi magni a voluptatum, vitae, consequuntur rerum illum odit fugit assumenda rem dolores inventore iste reprehenderit maxime! Iusto.</p>
   </Layout>
 </template>
 
@@ -17,16 +10,6 @@ export default {
   }
 }
 </script>
+
 <style lang="scss">
-h1 {
-  background: red;
-}
-
-ul {
-  background: green;
-  li {
-    color: purple;
-  }
-}
-
 </style>
