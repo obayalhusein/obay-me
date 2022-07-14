2d20289d9f82665131f8e44c468bec9273498bc9
obayalhusein <obayalhusein@gmail.com>
2022-07-14 11:58:54 +0300

    finish home animation

--- src/components/vector/icons/IconHome.vue
+++ src/components/vector/icons/IconHome.vue
@@ -1,12 +1,10 @@
 <template>
     <div>
         <svg class="nav-icon icon-home" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 135.2 126.5">
-            <g class="home-body">
-                <g>
-                    <path class="stroke" d="M58.6,64.1H76.2a12,12,0,0,1,12,12v47.4a0,0,0,0,1,0,0H46.6a0,0,0,0,1,0,0V76.1A12,12,0,0,1,58.6,64.1Z"/>
-                    <circle class="fill" cx="61.9" cy="93.8" r="3.8"/>
-                </g>
-                <path class="stroke" d="M1018.9,1149.9v52.5a8,8,0,0,1-8,8H931.5a8,8,0,0,1-8-8v-52.5l43-35.8a7.3,7.3,0,0,1,9.4,0Z" transform="translate(-903.6 -1086.9)"/>
+            <path class="stroke" d="M1018.9,1149.9v52.5a8,8,0,0,1-8,8H931.5a8,8,0,0,1-8-8v-52.5l43-35.8a7.3,7.3,0,0,1,9.4,0Z" transform="translate(-903.6 -1086.9)"/>
+            <g class="home-door">
+                <path class="stroke" d="M58.6,64.1H76.2a12,12,0,0,1,12,12v47.4a0,0,0,0,1,0,0H46.6a0,0,0,0,1,0,0V76.1A12,12,0,0,1,58.6,64.1Z"/>
+                <circle class="fill" cx="61.9" cy="93.8" r="3.8"/>
             </g>
             <g class="home-roof">
                 <path class="stroke" d="M1035.8,1145.2c0,7.3-9.1,11.2-14.9,6.4l-44.3-37a7.8,7.8,0,0,0-10.8,0l-44.4,37c-5.8,4.8-14.8.9-14.8-6.4h0a8.3,8.3,0,0,1,3-6.3l56.2-46.9a8,8,0,0,1,10.8,0l56.1,46.9a8.4,8.4,0,0,1,3.1,6.3Z" transform="translate(-903.6 -1086.9)"/>
@@ -25,35 +23,21 @@ export default {
 </script>
 
 <style lang="scss">
-
 .nav-link {
     &:hover {
         .nav-icon.icon-home {
-            .home-body,
+            .home-roof,
+            .home-door,
             .smoker {
                 animation: icon-element-jump .6s forwards;
             }
-            .home-roof {
-                transform-origin: center;
-                animation: icon-roof-jump .6s forwards;
-            }
+            .home-roof {animation-delay: .2s}
+            .smoker {animation-delay: .4s}
         }
     }
 }
 
 @keyframes icon-element-jump {
     50% { transform: translateY(-10px) }
-}
-
-@keyframes icon-roof-jump {
-    25% {
-        transform: translate(-2.5%, -5%) rotate(-5deg);
-    }
-    50% {
-        transform: translate(2.5%, -10%) rotate(5deg);
-    }
-    75% {
-        transform: translate(-2.5%, -7.5%) rotate(-5deg);
-    }
 }
 </style>
