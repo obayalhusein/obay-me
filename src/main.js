8fb85767bf21803fd0a3aff985e49f1d2d6baaa5
obayalhusein <obayalhusein@gmail.com>
2022-07-17 23:44:47 +0300

    change importing links at main.js

--- src/main.js
+++ src/main.js
@@ -1,19 +1,19 @@
 // This is the main.js file. Import global CSS and scripts here.
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
-import DefaultLayout from '~/layouts/Default.vue'
-import '~/assets/scss/app.scss'
+import DefaultLayout from '@/layouts/Default.vue'
+import '@/assets/scss/app.scss'
 
 // Vuex store
 import Vuex from 'vuex'
-import store from '~/store'
+import store from '@/store'
 
 // Global Components
-import Button from '~/components/ui/Button.vue'
-import Input from '~/components/ui/Input.vue'
-import Img from '~/components/ui/Img.vue'
-import Row from '~/components/ui/Row.vue'
-import Col from '~/components/ui/Col.vue'
+import Button from '@/components/ui/Button.vue'
+import Input from '@/components/ui/Input.vue'
+import Img from '@/components/ui/Img.vue'
+import Row from '@/components/ui/Row.vue'
+import Col from '@/components/ui/Col.vue'
 
 export default function (Vue, { router, head, isClient, appOptions }) {
   Vue.use(Vuex)
