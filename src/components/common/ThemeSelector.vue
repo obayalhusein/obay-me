4910dbf1b78fe53b9d2167ed094feeac0b7afff9
obayalhusein <obayalhusein@gmail.com>
2022-07-18 01:28:26 +0300

    hide theme tooltip on load

--- src/components/common/ThemeSelector.vue
+++ src/components/common/ThemeSelector.vue
@@ -22,7 +22,7 @@ export default {
     components: { IconMagic, ThemeSelectorTooltip },
     data () {
         return {
-            showThemeSelector: true,
+            showThemeSelector: false,
         }
     },
 }
